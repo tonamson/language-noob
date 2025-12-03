a82b05b24f85e359d665e435d4ecec530a0cc68e
tonamson <tonamson4@gmail.com>
2025-12-03 16:15:28 +0700

    Fix config serrvices

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TranslateModule } from './translate/translate.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TranslateModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TranslateModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
